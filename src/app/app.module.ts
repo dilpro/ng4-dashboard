9009bf5bb7ea0754afb2805f79eebb72800bc29d
Dilanka <dilankabc@gmail.com>
2017-06-23 22:46:49 +0530

    Add Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MdSidenavModule} from '@angular/material';
+import {MdInputModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdSidenavModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
